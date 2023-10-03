2f2a61a6129d1c574842f85788e0be7d284092b7
Gonzalo Mena-Mendoza <gonzalo@mena.com.mx>
2023-10-02 20:01:40 -0600

    Add first controller unit test

--- src/test/java/connectfour/controller/ConnectFourControllerTests.java
+++ src/test/java/connectfour/controller/ConnectFourControllerTests.java
@@ -1,13 +1,30 @@
 package connectfour.controller;
 
-import connectfour.controller.ConnectFourController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ConnectFourControllerTests {
 
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    public void shouldCreateNewGame() throws Exception{
+        this.mockMvc.perform(post("/connectfour"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.nextPlayer").value(1))
+                .andExpect(jsonPath("$.winner").value(0))
+        ;
+    }
 }
